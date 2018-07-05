a84646a9c5b71c98fe3cd2b689529e79693b851b
sunfangzhou <fangzhou.sun@mediacorp.com.sg>
2018-07-05 09:51:05 +0800

    add babelrc & libs; apply styl

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import App from './App'
-import './index.styl'
 import registerServiceWorker from './registerServiceWorker'
+import './styles/index.styl'
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
 registerServiceWorker()
